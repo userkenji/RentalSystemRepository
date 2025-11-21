ea616c824f9af930af868f792ad0c02978767b55
ichiasa-g-3 <ichiasa-g-3@example.com>
2025-11-21 14:29:29 +0900

    FirstCommit

---
+++ src/main/java/com/ikeda/RentalSystemProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ikeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalSystemProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalSystemProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ikeda/RentalSystemProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikeda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalSystemProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
